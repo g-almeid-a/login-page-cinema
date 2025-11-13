50c462a0fe228c75035b04c61f91b862828dff8b
g-almeid-a <ga.almeida1923@gmail.com>
2025-11-13 16:56:40 -0300

    new web page

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o container principal
+const authBox = document.getElementById('authBox');
+
+// Função para alternar entre os formulários de login e registro
+function toggleForm() {
+    authBox.classList.toggle('ativo');
+}
